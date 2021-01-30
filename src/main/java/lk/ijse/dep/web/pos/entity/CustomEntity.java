ab9ca37c02a1855932a3676bc3709e07f6da55ef
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-01-30 13:54:22 +0530

    Add CustomEntity.java

---
+++ src/main/java/lk/ijse/dep/web/pos/entity/CustomEntity.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep.web.pos.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomEntity implements SuperEntity {
+}
