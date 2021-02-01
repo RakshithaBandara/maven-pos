8fa0f20b17e95c8b7c509758700611cbd95cb3c1
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-02-01 13:54:14 +0530

    Finalize API Layer

---
+++ src/main/java/lk/ijse/dep/web/pos/api/CustomerServlet.java
@@ -0,0 +1,150 @@
+package lk.ijse.dep.web.pos.api;
+
+import lk.ijse.dep.web.pos.business.BOFactory;
+import lk.ijse.dep.web.pos.business.BOTypes;
+import lk.ijse.dep.web.pos.business.custom.CustomerBO;
+import lk.ijse.dep.web.pos.dto.CustomerDTO;
+import lk.ijse.dep.web.pos.exception.HttpResponseException;
+import lk.ijse.dep.web.pos.exception.ResponseExceptionUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@WebServlet(urlPatterns = "/api/v1/customers/*")
+public class CustomerServlet extends HttpServlet {
+
+    final Logger logger = LoggerFactory.getLogger(CustomerServlet.class);
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            super.service(req, resp);
+        } catch (Throwable t) {
+            ResponseExceptionUtil.handle(t, resp);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        EntityManager em = emf.createEntityManager();
+
+        try{
+
+            if (req.getPathInfo() == null || req.getPathInfo().replace("/", "").trim().isEmpty()) {
+                throw new HttpResponseException(400, "Invalid customer id", null);
+            }
+
+            String id = req.getPathInfo().replace("/", "");
+
+            CustomerBO customerBO = BOFactory.getInstance().getBO(BOTypes.CUSTOMER);
+            customerBO.setEntityManager(em);
+            customerBO.deleteCustomer(id);
+            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        final EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        EntityManager em = emf.createEntityManager();
+
+        try{
+
+            if (req.getPathInfo() == null || req.getPathInfo().replace("/", "").trim().isEmpty()) {
+                throw new HttpResponseException(400, "Invalid customer id", null);
+            }
+
+            String id = req.getPathInfo().replace("/", "");
+            Jsonb jsonb = JsonbBuilder.create();
+            CustomerDTO dto = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+
+            if (dto.getId() != null || dto.getName().trim().isEmpty() || dto.getAddress().trim().isEmpty()) {
+                throw new HttpResponseException(400, "Invalid details", null);
+            }
+
+            CustomerBO customerBO = BOFactory.getInstance().getBO(BOTypes.CUSTOMER);
+            customerBO.setEntityManager(em);
+            customerBO.updateCustomer(dto);
+            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+
+        } catch (JsonbException exp) {
+            throw new HttpResponseException(400, "Failed to read the JSON", exp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Jsonb jsonb = JsonbBuilder.create();
+
+        final EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        EntityManager em = emf.createEntityManager();
+
+        try{
+            resp.setContentType("application/json");
+            CustomerBO customerBO = BOFactory.getInstance().getBO(BOTypes.CUSTOMER);
+            customerBO.setEntityManager(em);
+            resp.getWriter().println(jsonb.toJson(customerBO.findAllCustomers()));
+
+        } catch (Throwable t) {
+            ResponseExceptionUtil.handle(t, resp);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Jsonb jsonb = JsonbBuilder.create();
+        final EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        EntityManager em = emf.createEntityManager();
+
+        try{
+            CustomerDTO dto = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+
+            if (dto.getId() == null || dto.getId().trim().isEmpty() || dto.getName() == null || dto.getName().trim().isEmpty() || dto.getAddress() == null || dto.getAddress().trim().isEmpty()) {
+                throw new HttpResponseException(400, "Invalid customer details", null);
+            }
+
+            CustomerBO customerBO = BOFactory.getInstance().getBO(BOTypes.CUSTOMER);
+            customerBO.setEntityManager(em);
+            customerBO.saveCustomer(dto);
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            resp.setContentType("application/json");
+            resp.getWriter().println(jsonb.toJson(dto));
+        } catch (SQLIntegrityConstraintViolationException exp) {
+            throw new HttpResponseException(400, "Duplicate entry", exp);
+        } catch (JsonbException exp) {
+            throw new HttpResponseException(400, "Failed to read the JSON", exp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally {
+            em.close();
+        }
+
+    }
+}
